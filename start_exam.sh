4bfe60a20ac9385935aa0c316db30c66637dbc5d
Riccardo Tommasini <riccardo.tommasini@polimi.it>
2018-07-03 14:25:35 +0200

    number of events

--- start_exam.sh
+++ start_exam.sh
@@ -4,7 +4,6 @@ sh /home/training/ksql/bin/ksql-datagen schema=/home/training/customers.avro for
 
 echo "Press Enter if the prompt control does not return"
 
-sh /home/training/ksql/bin/ksql-datagen -daemon schema=/home/training/ratings.avro format=json topic=ratings key=rating_id  maxInterval=10 iterations=1000 && true
-
+sh /home/training/ksql/bin/ksql-datagen -daemon schema=/home/training/ratings.avro format=json topic=ratings key=rating_id  maxInterval=10 iterations=50000
 
 /home/training/ksql/bin/ksql-cli local --properties-file /home/training/ksql/ksql.properties
